aed84b3f2b6a5030865164388270743f80333a4d
botdagalera <pjt1991@gmail.com>
2022-03-08 11:48:04 +0000

    Updated infrastructue for domain DataPlatform

--- DataPlatform/prod/locals.tf
+++ DataPlatform/prod/locals.tf
@@ -8,8 +8,8 @@ locals {
         "contains_pii": false,
         "amplitude": false,
         "emr": {
-            "master_instance_type": "m5.medium",
-            "core_instance_type": "m5.medium",
+            "master_instance_type": "m5.2xlarge",
+            "core_instance_type": "m5.2xlarge",
             "core_instance_count": 5,
             "release_label": "emr-4.6.0",
             "applications": [
